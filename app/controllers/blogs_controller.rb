07cd4ca50b9d6092355b3932b4548207609103f5
Junghoon Lee <scarlet3628@likelion.org>
2017-12-13 08:26:35 +0000

    Banner&Blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,29 @@
+class BlogsController < ApplicationController
+    def blog_01
+      @blog_sidebar = Ecommerce.all
+      @sidebanner_01 = @blog_sidebar.sample
+      @sidebanner_02 = @blog_sidebar.sample
+      @sidebanner_03 = @blog_sidebar.sample
+      @sidebanner_04 = @blog_sidebar.sample
+      @sidebanner_05 = @blog_sidebar.sample
+      @sidebanner_06 = @blog_sidebar.sample
+      @sidebanner_07 = @blog_sidebar.sample
+      @sidebanner_08 = @blog_sidebar.sample
+      @sidebanner_09 = @blog_sidebar.sample
+      @sidebanner_10 = @blog_sidebar.sample
+    end
+    
+    def blog_02
+      @blog_sidebar = Ecommerce.all
+      @sidebanner_01 = @blog_sidebar.sample
+      @sidebanner_02 = @blog_sidebar.sample
+      @sidebanner_03 = @blog_sidebar.sample
+      @sidebanner_04 = @blog_sidebar.sample
+      @sidebanner_05 = @blog_sidebar.sample
+      @sidebanner_06 = @blog_sidebar.sample
+      @sidebanner_07 = @blog_sidebar.sample
+      @sidebanner_08 = @blog_sidebar.sample
+      @sidebanner_09 = @blog_sidebar.sample
+      @sidebanner_10 = @blog_sidebar.sample
+    end
+end
--- app/controllers/hotdeals_controller.rb
+++ app/controllers/hotdeals_controller.rb
@@ -2,45 +2,4 @@ class HotdealsController < ApplicationController
    def hotdeal
    end
   
-   def deal_01
-      @searchbanner = Ecommerce.all
-      @sidebanner_01 = @searchbanner.sample
-      @sidebanner_02 = @searchbanner.sample
-      @sidebanner_03 = @searchbanner.sample
-      @sidebanner_04 = @searchbanner.sample
-      @sidebanner_05 = @searchbanner.sample
-      @sidebanner_06 = @searchbanner.sample
-      @sidebanner_07 = @searchbanner.sample
-      @sidebanner_08 = @searchbanner.sample
-      @sidebanner_09 = @searchbanner.sample
-      @sidebanner_10 = @searchbanner.sample
-    end
-    
-    def deal_02
-      @searchbanner = Ecommerce.all
-      @sidebanner_01 = @searchbanner.sample
-      @sidebanner_02 = @searchbanner.sample
-      @sidebanner_03 = @searchbanner.sample
-      @sidebanner_04 = @searchbanner.sample
-      @sidebanner_05 = @searchbanner.sample
-      @sidebanner_06 = @searchbanner.sample
-      @sidebanner_07 = @searchbanner.sample
-      @sidebanner_08 = @searchbanner.sample
-      @sidebanner_09 = @searchbanner.sample
-      @sidebanner_10 = @searchbanner.sample
-    end
-    
-    def deal_03
-      @searchbanner = Ecommerce.all
-      @sidebanner_01 = @searchbanner.sample
-      @sidebanner_02 = @searchbanner.sample
-      @sidebanner_03 = @searchbanner.sample
-      @sidebanner_04 = @searchbanner.sample
-      @sidebanner_05 = @searchbanner.sample
-      @sidebanner_06 = @searchbanner.sample
-      @sidebanner_07 = @searchbanner.sample
-      @sidebanner_08 = @searchbanner.sample
-      @sidebanner_09 = @searchbanner.sample
-      @sidebanner_10 = @searchbanner.sample
-    end
 end
